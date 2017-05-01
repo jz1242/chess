c87a61ef6cb3c2505adff472855d418a421cfdc2
Kristin Yim <k.yim33@gmail.com>
2017-04-30 20:39:55 -0400

    updated with fixed rook

--- Board.cpp
+++ Board.cpp
@@ -78,7 +78,6 @@ Piece* Board::newPiece(int id, Player owner) {
     }
 }
 
-<<<<<<< HEAD
 int Board::checkValidRow(Position start, Position end) const {
     int moveToRight = 0;
     if(start.y < end.y){
@@ -103,5 +102,3 @@ int Board::checkValidRow(Position start, Position end) const {
     }
     return 1;
 }
-=======
->>>>>>> f54c48d9e01780daa13abf01307fb9e34fb78c58
