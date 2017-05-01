8d85bc4cdfafb22bba5316f04781e1cfb4f99d81
Jason Zhang <jz1242@gmail.com>
2017-04-30 20:31:25 -0400

    Deleted checkRow method, fixed merge conflicts, added notes on how to properly index board

--- Chess.cpp
+++ Chess.cpp
@@ -48,15 +48,11 @@ int Pawn::validMove(Position start, Position end,
     }
     return SUCCESS;
 }
-<<<<<<< HEAD
+
 int Rook::validMove(Position start, Position end,
         const Board& board) const{ 
-    if(!board.ChessGame::checkRow(start, end)){
-        return 0;
-    }
     return SUCCESS;
 }
-=======
 
 bool checkRow(Position start, Position end){
     if(!(start.x == end.x)){
@@ -68,7 +64,6 @@ bool checkRow(Position start, Position end){
     return true;
 }
 
->>>>>>> 1a9d1f6bf84cf6831255271ab7744227f8b1697d
 int Knight::validMove(Position start, Position end,
     const Board& board) const { 
     if(!(
@@ -136,25 +131,19 @@ int main() {
     std::cout << std::endl;
     chess.makeMove(Position(4, 0), Position(4, 1));
     printAllPieces(&chess);
-<<<<<<< HEAD
 
-   /* std::cout <<chess.turn()<< std::endl;
+    std::cout <<chess.turn()<< std::endl;
     chess.makeMove(Position(1, 1), Position(1, 2));
-=======
- */
->>>>>>> 4f426a681b67d8ef06a76b58a55cafda903c2402
+
+
     printAllPieces(&chess);
     std::cout <<chess.turn()<< std::endl;
-<<<<<<< HEAD
-    chess.makeMove(Position(1, 0), Position(0, 2));
+   // chess.makeMove(Position(1, 0), Position(0, 2));
     printAllPieces(&chess);
-    std::cout <<chess.turn()<< std::endl;*/
-=======
-
+    std::cout <<chess.turn()<< std::endl;
     std::cout << std::endl;
-    chess.makeMove(Position(0, 1), Position(0, 0));
+    chess.makeMove(Position(1, 0), Position(2, 2));
     printAllPieces(&chess);
 
->>>>>>> 4f426a681b67d8ef06a76b58a55cafda903c2402
 
 }
